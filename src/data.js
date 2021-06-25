e1c90c93c11949b2bad0c05b3dfc1616a0061a8a
Vanshika <vanshikasikarwar79@gmail.com>
2021-06-26 02:47:39 +0530

    Updated Index and data file

--- src/data.js
+++ src/data.js
@@ -1,76 +1,76 @@
 export const herbData = [
     {
-      imgsrc: " ../Assets/herbs/aloevera2.jpg",
+      imgsrc: "./Assets/herbs/aloevera2.jpg",
       title: "Aloe Vera",
       description: "",
     },
     {
-      imgsrc:"../Assets/herbs/garlic.jpg",
+      imgsrc:"./Assets/herbs/garlic.jpg",
       title:"Garlic",
       description:""
     },
     {
-     imgsrc:"../Assets/herbs/ginger2.jpg",
+     imgsrc:"./Assets/herbs/ginger2.jpg",
      title:"Ginger",
      description:""
     },
     {
-      imgsrc: "../Assets/herbs/hibiscus.jpg",
+      imgsrc: "./Assets/herbs/hibiscus.jpg",
       title: "Hibiscus",
       description:""
     },
     {
-      imgsrc:"../Assets/herbs/lavender.jpg",
+      imgsrc:"./Assets/herbs/lavender.jpg",
       title:"Lavender",
       description:""
     },
     {
-      imgsrc:"../Assets/herbs/junniperberry.jpg",
+      imgsrc:"./Assets/herbs/junniperberry.jpg",
       title:"Juniper Berry",
       description:""
     },
     {
-        imgsrc:"../Assets/herbs/mint.jpg",
+        imgsrc:"./Assets/herbs/mint.jpg",
         title:"Mint",
         description:""
       },
       {
-        imgsrc:"../Assets/herbs/oregano.jpg",
+        imgsrc:"./Assets/herbs/oregano.jpg",
         title:"Oregano",
         description:""
       },
       {
-        imgsrc:"../Assets/herbs/resemary.jpg",
+        imgsrc:"./Assets/herbs/resemary.jpg",
         title:"Rose Mary",
         description:""
       },
       {
-        imgsrc:"../Assets/herbs/Saffron.jpg",
+        imgsrc:"./Assets/herbs/Saffron.jpg",
         title:"Saffron",
         description:""
       },
       {
-        imgsrc:"../Assets/herbs/sage.jpg",
+        imgsrc:"./Assets/herbs/sage.jpg",
         title:"Sage",
         description:""
       },
       {
-      imgsrc:"../Assets/herbs/turmeric.jpg",
+      imgsrc:"./Assets/herbs/turmeric.jpg",
       title:"Turmeric",
       description:""
     },
     {
-        imgsrc:"../Assets/herbs/weed.jpg",
+        imgsrc:"./Assets/herbs/weed.jpg",
         title:"Weed",
         description:""
       },
       {
-        imgsrc:"../Assets/herbs/lemonverbena.jpg",
+        imgsrc:"./Assets/herbs/lemonverbena.jpg",
         title:"Lemon Verbena",
         description:""
       },
       {
-        imgsrc:"../Assets/herbs/poppyseeds.jpg",
+        imgsrc:"./Assets/herbs/poppyseeds.jpg",
         title:"Poppy Seeds",
         description:""
       },
