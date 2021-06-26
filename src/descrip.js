88ebf31261086d9bb7548b2d532916f3f01e4b10
Mansi Jain <mansijain1519@gmail.com>
2021-06-27 03:27:45 +0530

    JSON

---
+++ src/descrip.js
@@ -0,0 +1,172 @@
+{
+"herbs" : [
+{
+"Aromatic" : [ 
+{
+imgsrc: 
+title: "Peppery Mint",
+},
+{
+imgsrc:
+title: "Sage",
+},
+{
+imgsrc: 
+title: "Rosemary",
+},
+{
+imgsrc:
+title: "Thyme",
+},
+{
+imgsrc:
+title: "Coriander",
+},
+{
+imgsrc: "",
+title: "Oregano",
+},
+]
+
+
+"Culinary" : [
+{
+imgsrc: 
+title: "Cumin",
+},
+{
+imgsrc:
+title: "Fenugreek",
+},
+{
+imgsrc: 
+title: "Juniper Berry",
+},
+{
+imgsrc:
+title: "Garlic",
+},
+{
+imgsrc:
+title: "Ginger",
+},
+{
+imgsrc: "",
+title: "Turmeric",
+},
+]
+
+"Annuals" : [
+{
+imgsrc: 
+title: "Basil",
+},
+{
+imgsrc:
+title: "Chervil",
+},
+{
+imgsrc: 
+title: "Cilantro",
+},
+{
+imgsrc:
+title: "Summer savory",
+},
+{
+imgsrc:
+title: "Dill",
+},
+{
+imgsrc: "",
+title: "Bay Laurel",
+},
+]
+
+"Medicinal" : [
+{
+imgsrc: 
+title: "Hena",
+},
+{
+imgsrc:
+title: "Calendula",
+},
+{
+imgsrc: 
+title: "Aloevera",
+},
+{
+imgsrc:
+title: "Ashwangandha",
+},
+{
+imgsrc:
+title: "Amla",
+},
+{
+imgsrc: "",
+title: "Sandalwood",
+},
+]
+
+
+"Biennials" : [
+{
+imgsrc: 
+title: "Parsley",
+},
+{
+imgsrc:
+title: "Stevia",
+},
+{
+imgsrc: 
+title: "Mullein",
+},
+{
+imgsrc:
+title: "Sweet William",
+},
+{
+imgsrc:
+title: "Carrot",
+},
+{
+imgsrc: "",
+title : "Susan",
+},
+]
+
+"Perennials" : [
+{
+imgsrc: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/closeup-of-lemon-verbena-herb-garden-royalty-free-image-1589569325.jpg?crop=0.672xw:1.00xh;0.328xw,0&resize=768:*",
+title: "Lemon Verbena",
+},
+{
+imgsrc: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/close-up-of-organic-fennel-plants-growing-on-rural-royalty-free-image-1589568929.jpg?crop=0.665xw:1.00xh;0.155xw,0&resize=768:*",
+title: "Fennel",
+},
+{
+imgsrc: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/fresh-lemon-balm-royalty-free-image-1589568984.jpg?crop=0.621xw:1.00xh;0.139xw,0&resize=768:*",
+title: "Lemon Balm",
+},
+{
+imgsrc: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/winter-savory-herb-satureja-montana-in-early-summer-royalty-free-image-1589570849.jpg?crop=0.66635xw:1xh;center,top&resize=768:*",
+title: "Winter savory",
+},
+{
+imgsrc: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/heartsease-viola-tricolor-news-photo-629381987-1542479575.jpg?crop=1xw:1xh;center,top&resize=980:*",
+title: "Viola",
+},
+{
+imgsrc: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sorrel-royalty-free-image-957629362-1542480600.jpg?crop=0.66658xw:1xh;center,top&resize=768:*",
+title: "Sorrel",
+},
+]
+},
+]
+},
+
+
+
