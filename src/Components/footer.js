356c0a91325c3554962b45c09a7fce195e9d21f8
Vanshika <vanshikasikarwar79@gmail.com>
2021-06-26 14:32:43 +0530

    Added Some Changes in footer

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -4,112 +4,30 @@ import './footer.css';
 
 function Footer() {
     return (
-  <footer className="page-footer fixed-bottom font-small stylish-color-dark pt-4">
-    <div className="container text-center text-md-left">
+    <footer className="bg-success footer">
+    <div className="container py-5">
       <div className="row">
-        <div className="col-md-4 mx-auto">
-          <h5 className="font-weight-bold text-uppercase mt-3 mb-4">Footer Content</h5>
-          <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
-            consectetur
-            adipisicing elit.</p>
+        <div className="col-lg-4 text-white text-center">
+          <p>By experimenting with various types of herbs you will become familiar with those herbs and the impact they have on the end use.</p>
         </div>
-        <hr className="clearfix w-100 d-md-none" />
-        <div className="col-md-2 mx-auto">
-          <h5 className="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
-          <ul className="list-unstyled">
-            <li>
-              <a href="#!">Link 1</a>
-            </li>
-            <li>
-              <a href="#!">Link 2</a>
-            </li>
-            <li>
-              <a href="#!">Link 3</a>
-            </li>
-            <li>
-              <a href="#!">Link 4</a>
-            </li>
-          </ul>
+        <div className="col-lg-4 text-white text-center">
+          <h4>Follow Us on:</h4>
+           <ul className="list-unstyled">
+
+           </ul>
         </div>
-        <hr className="clearfix w-100 d-md-none" />
-        <div className="col-md-2 mx-auto">
-          <h5 className="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
-          <ul className="list-unstyled">
-            <li>
-              <a href="#!">Link 1</a>
-            </li>
-            <li>
-              <a href="#!">Link 2</a>
-            </li>
-            <li>
-              <a href="#!">Link 3</a>
-            </li>
-            <li>
-              <a href="#!">Link 4</a>
-            </li>
-          </ul>
+        <div className="col-lg-4">
+          <form className="input-group">
+          <input type="text" className="form-control input-style form-control-sm bg-success text-center text-white" placeholder="You@domain.com" />
+          <div className="input-group-append">
+          <button type="button" className="btn btn-sm btn-style">Register</button>
         </div>
-        <hr className="clearfix w-100 d-md-none" />
-        <div className="col-md-2 mx-auto">
-          <h5 className="font-weight-bold text-uppercase mt-3 mb-4">Links</h5>
-          <ul className="list-unstyled">
-            <li>
-              <a href="#!">Link 1</a>
-            </li>
-            <li>
-              <a href="#!">Link 2</a>
-            </li>
-            <li>
-              <a href="#!">Link 3</a>
-            </li>
-            <li>
-              <a href="#!">Link 4</a>
-            </li>
-          </ul>
+        </form>
         </div>
       </div>
     </div>
-    <hr />
-    <ul className="list-unstyled list-inline text-center py-2">
-      <li className="list-inline-item">
-        <h5 className="mb-1">Register for free</h5>
-      </li>
-      <li className="list-inline-item">
-        <a href="#!" className="btn btn-danger btn-rounded">Sign up!</a>
-      </li>
-    </ul>
-    <hr />
-    <ul className="list-unstyled list-inline text-center">
-      <li className="list-inline-item">
-        <a className="btn-floating btn-fb mx-1">
-          <i className="fab fa-facebook-f"> </i>
-        </a>
-      </li>
-      <li className="list-inline-item">
-        <a className="btn-floating btn-tw mx-1">
-          <i className="fab fa-twitter"> </i>
-        </a>
-      </li>
-      <li className="list-inline-item">
-        <a className="btn-floating btn-gplus mx-1">
-          <i className="fab fa-google-plus-g"> </i>
-        </a>
-      </li>
-      <li className="list-inline-item">
-        <a className="btn-floating btn-li mx-1">
-          <i className="fab fa-linkedin-in"> </i>
-        </a>
-      </li>
-      <li className="list-inline-item">
-        <a className="btn-floating btn-dribbble mx-1">
-          <i className="fab fa-dribbble"> </i>
-        </a>
-      </li>
-    </ul>
-    <div className="footer-copyright text-center py-3">© 2020 Copyright:
-      <a href="https://mdbootstrap.com/"> MDBootstrap.com</a>
-    </div>
-  </footer>
+    <div className="container-fluid text-center text-white">Copyright © Framer Inc. All rights reserved.</div>
+    </footer>
 );
 }
 
