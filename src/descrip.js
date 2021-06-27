4436dd1ec621420e1154c42f361f57dfe018d790
Mansi Jain <mansijain1519@gmail.com>
2021-06-27 12:31:05 +0530

    Description pages json

--- src/descrip.js
+++ src/descrip.js
@@ -28,81 +28,81 @@ title: "Oregano"
 
 export const cullinaryData =[
 {
-imgsrc : "", 
-title: "Cumin",
+imgsrc : "https://lh3.googleusercontent.com/iJeuSUYdQGmO0NHfu1EbMQouNF17aajC8e51aQ4SutDJA-SFdIl4DWqHycaXYalfezCxXUE=s101",
+title: "Cumin"
 },
 {
-imgsrc : "",
+imgsrc : "https://lh3.googleusercontent.com/_p63wkHzUg_ETc6KlCRLRzZiS3F-KR1GFW9ZfGDXt6Lj5Q5Xa1N3NNHMHyI77VvqbmbLZTQ=s85",
 title: "Fenugreek",
 },
 {
-imgsrc : "", 
+imgsrc : "https://lh3.googleusercontent.com/i5kk6FJQ6Mob-UC5XW1uLKe2UgtNJIDP6DSWnRzbCE2_EiTOynq9whnRaqCUJyfSAJe6fQ=s113", 
 title: "Juniper Berry",
 },
 {
-imgsrc : "",
+imgsrc : "https://lh3.googleusercontent.com/1L5-BRJ3eN2GYEjIVAyXWW2OzLJ2lQByuvJPg51HlIxFubYgEiPTghuK9U5jN9TrCzFtSaA=s113",
 title: "Garlic",
 },
 {
-imgsrc : "",
+imgsrc : "https://lh3.googleusercontent.com/7iAIvSTKoYDA5E9FcW58mOfmyVfT4Po2cQvOKnRPoMXith9hyu0iu-jkbtBesobwFHXwqQ=s128",
 title: "Ginger",
 },
 {
-imgsrc : "",
+imgsrc : "https://lh3.googleusercontent.com/nkF2IO6iaYXxaVQZAwiKs5MSRyPns1ZjaonHglKjFPcnd2EhGy0ZCbP6GSm2EbnXGKLrJQ=s128",
 title: "Turmeric",
 },
 ]
 
 export const annualData =[
 {
-imgsrc : "", 
+imgsrc : "https://lh3.googleusercontent.com/ibxrYG8G7T0ZeXhE26KUeGGYV2R4VM4MdeEwQNN8yHBGy63-HoIvdDrhfOMeXwRo0bcYEQ=s128", 
 title: "Basil",
 },
 {
-imgsrc : "",
+imgsrc : "https://lh3.googleusercontent.com/FTngIl54PPD600wIBlWWkLWe7lZrN9CNveGKm9aT5jfW__xQZ7h6X9Z7fB39BgEqkLEVrQ=s113",
 title: "Chervil",
 },
 {
-imgsrc : "", 
+imgsrc : "https://lh3.googleusercontent.com/TVIvE18gB9tQknOzdr05_gONhJefDYU4YRCViI-s8tqd0ZWpFQsRa0TP-xr0cVMjXGE8a9Y=s136",
 title: "Cilantro",
 },
 {
-imgsrc : "",
+imgsrc : "https://lh3.googleusercontent.com/WyY_WlnGoVEH87zl4lSDAfMo7OyAUMTZhcsOljArJ-4-NUk1lHLa8yLwSdZMWUQCZUjTMg=s128",
 title: "Summer savory",
 },
 {
-imgsrc : "",
+imgsrc : "https://lh3.googleusercontent.com/gMYEsIXG85GI-lktdHB8BXsSeroKPt3-knBKxnWQjkVM-9ZuvAozN7sSQcZH9fTqye3SSA=s85",
 title: "Dill",
 },
 {
-imgsrc : "",
+imgsrc : "https://lh3.googleusercontent.com/69uelP_1NS_weUYmIFLcOgUaIbBknp49Cr7YbtCk30d3H9QsgFVVzZiQ_aj_4HFWZFC4-w=s123",
 title: "Bay Laurel",
 },
 ]
 
 export const medicinalData =[ 
 {
-imgsrc : "", 
+imgsrc : "https://lh3.googleusercontent.com/1VjAlrr_3vWJSnc37b9mBYZkqQ7xzi_KQdr0DJtU8Muz_pJIRBaUDiK1qjjxq4Jk47K3=s85", 
 title: "Hena",
 },
 {
-imgsrc : "",
+imgsrc : "https://lh3.googleusercontent.com/XF3oDo4PPAEiKPruLrtAYmduuXvq8sQfM7ZmYRLKoL-epNqay22exTAg_YNclZQpmAQLqQ=s128",
 title: "Calendula",
 },
 {
-imgsrc : "", 
+imgsrc : "https://lh3.googleusercontent.com/VFtWB7hejNsbg25rlX8T-s5foMk3E7B2oTZlGnTX-KopGaUWrG3a1i7G4P13Q3yiJs5hTs4=s127", 
 title: "Aloevera",
 },
 {
-imgsrc : "",
+imgsrc : "https://lh3.googleusercontent.com/iFwzmDl2wFt3TFzcmY9UHyZmo8P1rTcM3UTIQRVUngGUd2dswc8Z7yNKTMEA0x2CkZX7kt8=s92",
 title: "Ashwangandha",
 },
 {
-imgsrc : "",
+imgsrc : "https://lh3.googleusercontent.com/7CFpCKUrD-VUzFaj25DQ77BXC-8A58nJArsPfFtne7ifjE6UyC4oc3rN0MNjwD1YXOGc=s113",
 title: "Amla",
 },
 {
-imgsrc : "", 
+imgsrc : "https://lh3.googleusercontent.com/Kc-KL1cfuNgUB4PEoo6va5euFxFLwxi8-dhARJfZ7VO-7EpkHeyWczxdA-imBtPXJPFq8A=s85", 
 title: "Sandalwood",
 },
 ]
@@ -110,27 +110,27 @@ title: "Sandalwood",
 
 export const biennialData  =[
 {
-imgsrc : "", 
+imgsrc : "https://lh3.googleusercontent.com/BRPO-WGS1gUoRqX3frXtFQu7xN0f4NQCCZeyyM6xnfEcGdw5LoHc-8lGc9TCSLLW1fOf=s113", 
 title: "Parsley",
 },
 {
-imgsrc : "",
+imgsrc : "https://lh3.googleusercontent.com/nJSI-P1hH2EfySnuahh1cKS0pp68-O-nUGK6QaADr0FYCcuGclr1WW7gUhLVwa91qbNwBLc=s128",
 title: "Stevia",
 },
 {
-imgsrc : "", 
+imgsrc : "https://lh3.googleusercontent.com/uob5n0tT6q3u21cwi_tQ7RtGZrY3PivMD9x1tMoXsKVUeBBoZ93XKnpBL0Oz6DamGeKY=s152", 
 title: "Mullein",
 },
 {
-imgsrc : "",
+imgsrc : "https://lh3.googleusercontent.com/QbOLF2T1GiA50w9FmWkXU7ARkH6IXLBF2af7Oib3CATXtMaBIKuoR9xVjfHv4cJUg7AuHw=s124",
 title: "Sweet William",
 },
 {
-imgsrc : "",
+imgsrc : "https://lh3.googleusercontent.com/Kv7fio9HgNUcZiwc329qo5PRY2AEjTj6yMxRxs6KFFzsW9SzJ6laOevvXoBhz5LDZHsgEw=s129",
 title: "Carrot",
 },
 {
-imgsrc : "",
+imgsrc : "https://lh3.googleusercontent.com/Ji9aq_qMb0gnEig6ax0kocovvGoBEdsJlfMPy0go7pGz2J9ZTSUYdtgR_2n5ANuj9mDBeQ=s144",
 title : "Susan",
 },
 ]
